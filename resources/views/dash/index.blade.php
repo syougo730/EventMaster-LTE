3a680a821845c2b84f93d6f80367a1f54601919b
shogo honma <honma@ad-tec.jp>
2022-01-05 00:46:26 +0900

    TOPにGoogleForm追加

--- resources/views/dash/index.blade.php
+++ resources/views/dash/index.blade.php
@@ -22,6 +22,8 @@
 
         {{-- ページ右側　内容が入ります --}}
 
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdppdAQRHlQUxS2CYIpSojEa0G8aysjmXGAZqZnt9AIYasTNQ/viewform?embedded=true" width="640" height="800" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+
       </div>
       <!-- /.container-fluid -->
     </div>
